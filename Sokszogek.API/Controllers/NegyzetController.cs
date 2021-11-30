e3a44c7df0a91ef6a0653d51831898829f7e19fa
Bence Boros <39780892+borosbence@users.noreply.github.com>
2021-11-30 19:50:36 +0100

    Add files via upload

---
+++ Sokszogek.API/Controllers/NegyzetController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sokszogek.Models;
+
+namespace Sokszogek.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NegyzetController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Kerulet")]
+        public double Kerulet(double oldalA)
+        {
+            var negyzet = new Negyzet(oldalA);
+            return negyzet.Kerulet();
+        }
+
+        [HttpGet]
+        [Route("Terulet")]
+        public double Terulet(double oldalA)
+        {
+            return 0;
+        }
+    }
+}
