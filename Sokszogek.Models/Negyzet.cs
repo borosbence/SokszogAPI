4f03ab084f9dfb5558d4ea0fb9a39d9f1adb1a35
Boros Bence <hello@bitben.hu>
2021-11-30 19:55:35 +0100

    init

---
+++ Sokszogek.Models/Negyzet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Sokszogek.Models
+{
+    public class Negyzet : Sokszog
+    {
+        public Negyzet(double A) : base(A)
+        {
+
+        }
+
+        public override double Kerulet()
+        {
+            if (OldalA <= 0)
+            {
+                return double.NaN;
+            }
+            return 4 * OldalA;
+        }
+
+        public override double Terulet()
+        {
+            if (OldalA <= 0)
+            {
+                return double.NaN;
+            }
+            return Math.Pow(OldalA, 2);
+        }
+    }
+}
